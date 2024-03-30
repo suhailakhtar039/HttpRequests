ba4e142aef1aa0701a23af2b62abdce5e1671274
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-30 16:46:28 +0530

    get service completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,33 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http.post("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json", postData);
+    this.http
+    .post("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json", postData)
+    .subscribe(responseData => {
+      console.log(responseData)
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http
+    .get("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json")
+    .subscribe(posts=>{
+      console.log(posts)
+    })
   }
 }
