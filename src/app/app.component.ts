c8182965ad49a68307151f821943284bee8803b1
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-31 23:12:34 +0530

    service created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators'
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService:PostsService) {}
 
   ngOnInit() {
     this.fetchPosts()
@@ -20,12 +20,7 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http
-    .post<{name:string}>
-    ("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json", postData)
-    .subscribe(responseData => {
-      console.log(responseData)
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content)
   }
 
   onFetchPosts() {
@@ -39,21 +34,6 @@ export class AppComponent implements OnInit {
 
   private fetchPosts(){
     this.isFetching = true
-    this.http
-    .get<{[key: string]:Post}>("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json")
-    .pipe(map((responseData) =>{
-      const postsArray: Post[] = [];
-      for(const key in responseData){
-        if(responseData.hasOwnProperty(key)){
-          postsArray.push({...responseData[key], id:key})
-        }
-      }
-      return postsArray;
-    }))
-    .subscribe(posts=>{
-      console.log(posts)
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    })
+    
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,12 +1,38 @@
+import { HttpClient } from "@angular/common/http";
+import {map} from 'rxjs/operators';
 import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
 
 @Injectable({providedIn:'root'})
 export class PostsService{
-    createAndStorePost(){
+    
+    constructor(private http: HttpClient){}
 
+    createAndStorePost(title:string, content:string){
+        const postData:Post = {title:title, content:content};
+
+        this.http
+        .post<{name:string}>
+        ("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json", postData)
+        .subscribe(responseData => {
+        console.log(responseData)
+        });
     }
 
     fetchPost(){
-        
+        this.http
+    .get<{[key: string]:Post}>("https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/posts.json")
+    .pipe(map((responseData) =>{
+      const postsArray: Post[] = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key], id:key})
+        }
+      }
+      return postsArray;
+    }))
+    .subscribe(posts=>{
+      console.log(posts)
+    })
     }
 }
