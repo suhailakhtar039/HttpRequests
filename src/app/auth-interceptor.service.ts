dd0936718e6051fbb68550e3257c61e22dbfe116
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-06 13:21:38 +0530

    auth interceptor completed

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,10 +1,21 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
-export class AuthInterceptorService implements HttpInterceptor{
+export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("first I'll come")
-        return next.handle(req);
+        const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xyz') });
+        return next.handle(modifiedRequest)
+            .pipe(
+                tap(event => {
+                    console.log(event);
+                    if (event.type === HttpEventType.Response) {
+                        console.log("Response arrived, body is");
+                        console.log(event.body)
+                    }
+                }
+                ));
     }
-    
+
 }
